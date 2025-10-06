faf9e7571ad86e75e4c03790129e4a004ac16687
mebdrey <mebdelrey@gmail.com>
2025-10-06 16:44:00 -0300

    funciona

--- frontend/web/src/components/Financeiro/CashFlow.jsx
+++ frontend/web/src/components/Financeiro/CashFlow.jsx
@@ -309,7 +309,7 @@ export function CashFlow({ categories, accountsPayable, accountsReceivable }) {
         </div>
       </div>
 
-      <Card className="w-100">
+      <Card className="w-100 ml-50">
         <CardHeader>
           <CardTitle>Fluxo de Caixa - {selectedYear}</CardTitle>
           <CardDescription>
