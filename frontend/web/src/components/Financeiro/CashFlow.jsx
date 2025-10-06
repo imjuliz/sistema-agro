ec22c10a5e799132e80c77d801173f94355b2a42
mebdrey <mebdelrey@gmail.com>
2025-10-06 17:08:24 -0300

    SORTE SORTE SORTE SORTE SORTE SORTE SORTE SORTE

--- frontend/web/src/components/Financeiro/CashFlow.jsx
+++ frontend/web/src/components/Financeiro/CashFlow.jsx
@@ -309,7 +309,7 @@ export function CashFlow({ categories, accountsPayable, accountsReceivable }) {
         </div>
       </div>
 
-      <Card className="w-380 ml-23 ">
+      <Card className="w-380 ml-20 ">
         <CardHeader>
           <CardTitle>Fluxo de Caixa - {selectedYear}</CardTitle>
           <CardDescription>
