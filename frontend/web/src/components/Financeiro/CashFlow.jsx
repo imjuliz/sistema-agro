79284836b3225cf4f71e35848900b989cebf7384
mebdrey <mebdelrey@gmail.com>
2025-10-06 16:58:21 -0300

    ml

--- frontend/web/src/components/Financeiro/CashFlow.jsx
+++ frontend/web/src/components/Financeiro/CashFlow.jsx
@@ -309,7 +309,7 @@ export function CashFlow({ categories, accountsPayable, accountsReceivable }) {
         </div>
       </div>
 
-      <Card className="w-380 ml-70 ">
+      <Card className="w-380 ml-50 ">
         <CardHeader>
           <CardTitle>Fluxo de Caixa - {selectedYear}</CardTitle>
           <CardDescription>
