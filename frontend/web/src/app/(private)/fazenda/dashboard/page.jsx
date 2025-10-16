ed448e5937102c71438435cd4a27e28226fcefb1
mebdrey <mebdelrey@gmail.com>
2025-10-16 17:18:41 -0300

    to tentando

--- frontend/web/src/app/(private)/fazenda/dashboard/page.jsx
+++ frontend/web/src/app/(private)/fazenda/dashboard/page.jsx
@@ -1,19 +1,26 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import FornecedoresTable from "@/components/Fornecedores"
-import { SectionCards, ChartPieDonut } from "@/components/Fazenda/dashboard"
+import { SectionCards, ChartPieDonut, SectionCards2 } from "@/components/Fazenda/dashboard"
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 export default function Page() {
-    return (
-        <div className="flex flex-1 flex-col p-10">
-            <h1 className="text-2xl font-semibold ml-10">Dashboard</h1>
-            <div className="@container/main flex flex-1 flex-col gap-2">
-            <div>
-                <SectionCards/>
-                <ChartPieDonut/>
-            </div>
-                <div className="justify-center W-100%">
-                    <FornecedoresTable />
-                </div>
-            </div>
+  return (
+    <div className="flex flex-1 flex-col p-10">
+      <h1 className="text-2xl font-semibold mb-6 ml-10">Dashboard</h1>
+
+      <div className="flex justify-between items-start gap-8 w-full">
+        <div className="flex-[0.3]"><SectionCards /></div>
+        <div className="flex-[0.4] flex justify-center"><ChartPieDonut /></div>
+        <div className="flex-[0.2] flex justify-end">
+          {/* <Stack spacing={2} direction="column" alignItems="end">
+            <Button variant="outlined">Outlined</Button>
+            <Button variant="outlined">Outlined2</Button>
+          </Stack> */}<SectionCards2/>
         </div>
-    )
+      </div>
+      <div className="mt-10 w-full"><FornecedoresTable /></div>
+    </div>
+  )
 }
+
--- frontend/web/src/components/Fazenda/dashboard.jsx
+++ frontend/web/src/components/Fazenda/dashboard.jsx
@@ -1,36 +1,88 @@
 "use client"
 import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
 import { Badge } from "@/components/ui/badge"
-import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle,CardContent } from "@/components/ui/card"
-import { TrendingUp } from "lucide-react"
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 import { Pie, PieChart } from "recharts"
-import {ChartContainer,ChartTooltip,ChartTooltipContent,} from "@/components/ui/chart"
+import { ChartContainer, ChartTooltip, ChartTooltipContent, } from "@/components/ui/chart"
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export function SectionCards() {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-1 gap-8 px-8 max-w-7xl mx-auto w-full">
-            <Card className="@container/card">
-                <CardHeader>
-                    <CardDescription>Lucros</CardDescription>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">R$6.790</CardTitle>
-                    <CardAction>
-                        <Badge variant="outline"><IconTrendingUp />+20%</Badge>
-                    </CardAction>
-                </CardHeader>
-                <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <div className="line-clamp-1 flex gap-2 font-medium">Lucros subiram em 20% <IconTrendingDown className="size-4" /></div>
-                    <div className="text-muted-foreground">Bom</div>
-                </CardFooter>
-            </Card>
-            <Card className="@container/card">
-                <CardHeader>
-                    <CardDescription>Parcerias Ativas</CardDescription>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">129</CardTitle>
-                </CardHeader>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-1 gap-8 px-8 max-w-7xl mx-auto w-full">
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Lucros</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">R$6.790</CardTitle>
+          <CardAction>
+            <Badge variant="outline"><IconTrendingUp />+20%</Badge>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">Lucros subiram em 20% <IconTrendingDown className="size-4" /></div>
+          <div className="text-muted-foreground">Bom</div>
+        </CardFooter>
+      </Card>
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Parcerias Ativas</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">129</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
+
+
+export function SectionCards2() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-8 w-full max-w-7xl mx-auto">
+      {/* Card 1 - Próxima Colheita */}
+      <div className="flex flex-col justify-between gap-4 w-full">
+        <Card className="flex-1 h-full p-6 min-h-[220px]">
+          <CardHeader>
+            <CardDescription>Próxima Colheita</CardDescription>
+            <CardTitle className="text-2xl font-semibold">Milho - 25/10</CardTitle>
+          </CardHeader>
+
+          <CardFooter className="flex-col items-start gap-1.5 text-sm">
+            <div className="flex gap-2 font-medium">
+              Lucros subiram em 20% <IconTrendingDown className="size-4" />
+            </div>
+            <div className="text-muted-foreground">Bom</div>
+          </CardFooter>
+        </Card>
+
+        <Stack spacing={2} direction="row" justifyContent="end">
+          <Button variant="outlined">OUTLINED</Button>
+          <Button variant="outlined">OUTLINED2</Button>
+        </Stack>
+      </div>
+
+      {/* Card 2 - Talhões Ativos */}
+      <div className="flex flex-col justify-between gap-4 w-full">
+        <Card className="flex-1 h-full p-6 min-h-[220px]">
+          <CardHeader>
+            <CardDescription>Talhões Ativos</CardDescription>
+            <CardTitle className="text-2xl font-semibold">37</CardTitle>
+          </CardHeader>
+
+          <CardFooter className="flex-col items-start gap-1.5 text-sm">
+            <div className="flex gap-2 font-medium">Lucros subiram em 20%</div>
+            <div className="text-muted-foreground">Bom</div>
+          </CardFooter>
+        </Card>
+
+        <Stack spacing={2} direction="row" justifyContent="end">
+          <Button variant="outlined">OUTLINED</Button>
+          <Button variant="outlined">OUTLINED2</Button>
+        </Stack>
+      </div>
+    </div>
+  );
+}
+
 
 export const description = "A donut chart"
 const chartData = [
@@ -42,31 +94,30 @@ const chartData = [
 ]
 
 const chartConfig = {
-  visitors: {label: "Visitors",},
-  chrome: {label: "Chrome",color: "var(--chart-1)",},
-  safari: {label: "Safari",color: "var(--chart-2)",},
-  firefox: {label: "Firefox",color: "var(--chart-3)",},
-  edge: {label: "Edge",color: "var(--chart-4)",},
-  other: {label: "Other",color: "var(--chart-5)",},
-} 
+  visitors: { label: "Visitors", },
+  chrome: { label: "Chrome", color: "var(--chart-1)", },
+  safari: { label: "Safari", color: "var(--chart-2)", },
+  firefox: { label: "Firefox", color: "var(--chart-3)", },
+  edge: { label: "Edge", color: "var(--chart-4)", },
+  other: { label: "Other", color: "var(--chart-5)", },
+}
 
 export function ChartPieDonut() {
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col w-full max-w-[500px] h-[400px]">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Lotes em andamento</CardTitle>
+        <CardDescription>750</CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[250px]">
-          <PieChart>
-            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />}/>
-            <Pie data={chartData} dataKey="visitors" nameKey="browser" innerRadius={60}/>
+
+      <CardContent className="flex-1 pb-4 flex justify-center items-center">
+        <ChartContainer config={chartConfig} className="w-[90%] h-[90%] flex justify-center items-center">
+          <PieChart width={300} height={300}>
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Pie data={chartData} dataKey="visitors" nameKey="browser" innerRadius={80} outerRadius={120}/>
           </PieChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-      </CardFooter>
     </Card>
   )
 }
