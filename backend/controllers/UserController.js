aeb60eca39e1e9c793cd94f2dd083436dcbfff09
julia <juliaalvesdeo447@gmail.com>
2025-11-11 15:37:20 -0300

    teste novo T-T

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -130,7 +130,7 @@ export async function loginController(req, res) {
     if (!email || !senha) return res.status(400).json({ error: 'Email e senha são obrigatórios' });
 
     // buscar usuário incluindo perfil
-    const user = await prisma.usuarios.findUnique({
+    const user = await prisma.usuario.findUnique({
       where: { email },
       include: { perfil: true },
     });
