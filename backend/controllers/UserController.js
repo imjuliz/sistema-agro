b57accd24b8031cc42b2a07e3cfa5e6be551c0e1
julia <juliaalvesdeo447@gmail.com>
2025-11-14 14:05:18 -0300

    teste

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -130,7 +130,7 @@ export async function loginController(req, res) {
       sameSite: isProd ? 'none' : 'lax',
       maxAge: REFRESH_TOKEN_DAYS * 24 * 60 * 60 * 1000,
       path: '/',
-      domain: isProd ? '.vercel.app' : undefined,
+      // domain: isProd ? '.vercel.app' : undefined,
     };
 
     // enviar cookie de refresh
@@ -251,7 +251,7 @@ export async function refreshController(req, res) {
       sameSite: isProd ? 'none' : 'lax',
       maxAge: REFRESH_TOKEN_DAYS * 24 * 60 * 60 * 1000,
       path: '/',
-      domain: isProd ? '.vercel.app' : undefined,
+      // domain: isProd ? '.vercel.app' : undefined,
     };
 
     // enviar novo cookie
