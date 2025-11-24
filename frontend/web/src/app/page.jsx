8bffd1f264d70b0eea3aeb0ae88bd94269982767
mebdrey <mebdelrey@gmail.com>
2025-11-24 16:30:47 -0300

    aaa

--- frontend/web/src/app/page.jsx
+++ frontend/web/src/app/page.jsx
@@ -6,8 +6,6 @@ import Hero from "../components/Home/sections/hero/default";
 import Items from "../components/Home/sections/items/default";
 import Logos from "../components/Home/sections/logos/default";
 import Navbar from "../components/Home/sections/navbar/default";
-import Pricing from "../components/Home/sections/pricing/default";
-import Stats from "../components/Home/sections/stats/default";
 import ThreeDMarqueeDemo from '@/components/Home/sections/socialProof/default'
 import { BentoSection } from "@/components/Home/sections/bentoGrid/default";
 
