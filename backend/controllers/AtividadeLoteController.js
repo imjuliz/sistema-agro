a46d4c3e4e80585ebbfbb2919dadc3d6743c144c
RS0usa1 <richardrrggts@gmail.com>
2025-11-24 17:17:51 -0300

    coisa

--- backend/controllers/AtividadeLoteController.js
+++ backend/controllers/AtividadeLoteController.js
@@ -18,4 +18,35 @@ export async function getAtividadeLoteTipoPlantioController(req, res) {
     }
 }
 
+export async function createAtividadeLoteController(req, res) {
+  try {
+    const data = req.body;
+    const { loteId } = req.params;
+    const responsavelId = req.usuario.id;
+
+    // Validacoes
+    if(!loteId || isNaN(loteId)) {
+      return res.status(400).json({erro: "Lote nao encontrado."})
+    }
+
+    if(!responsavelId || isNaN(responsavelId)) {
+      return res.status(400).json({erro: "Responsavel nao encontrado."})
+    }
+
+    const insert = await createAtividadeLote(data, loteId, responsavelId);
+
+    return res.status(201).json({
+      sucesso: true,
+      insert,
+      message: "Atividade do lote criada com sucesso!!"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucesso: false,
+      erro: "Erro ao criar atividade do lote",
+      detalhes: error.message
+    })
+  }
+}
+
 // função exportada para a rota de lote(loteRoutes.js)
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,30 +1,47 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/jwt.js"; // Importar a chave secreta
 
-import prisma from '../prisma/client.js';
-import dotenv from 'dotenv';
+import prisma from "../prisma/client.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const auth = (perfisPermitidos = []) => {
   return async (req, res, next) => {
-    const authHeader = req.headers.authorization || '';
-    const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
-    if (!token) return res.status(401).json({ mensagem: 'Não autorizado: Token não fornecido' });
-
+    const authHeader = req.headers.authorization || "";
+    const token = authHeader.startsWith("Bearer ")
+      ? authHeader.split(" ")[1]
+      : null;
+    if (!token)
+      return res
+        .status(401)
+        .json({ mensagem: "Não autorizado: Token não fornecido" });
 
     try {
       const decoded = jwt.verify(token, JWT_SECRET);
-      const user = await prisma.usuario.findUnique({ where: { id: decoded.id }, include: { perfil: true } });
-      if (!user) return res.status(401).json({ mensagem: 'Usuário não encontrado' });
+      const user = await prisma.usuario.findUnique({
+        where: { id: decoded.id },
+        include: { perfil: true },
+      });
+      if (!user)
+        return res.status(401).json({ mensagem: "Usuário não encontrado" });
 
-      if (decoded.tokenVersion !== user.tokenVersion) { return res.status(401).json({ mensagem: 'Token inválido (revogado)' }); }
+      if (decoded.tokenVersion !== user.tokenVersion) {
+        return res.status(401).json({ mensagem: "Token inválido (revogado)" });
+      }
 
-
-      if (perfisPermitidos.length > 0 && !perfisPermitidos.includes(user.perfil.nome)) { return res.status(403).json({ mensagem: 'Acesso negado para este perfil' }); }
+      if (
+        perfisPermitidos.length > 0 &&
+        !perfisPermitidos.includes(user.perfil.nome)
+      ) {
+        return res
+          .status(403)
+          .json({ mensagem: "Acesso negado para este perfil" });
+      }
 
       req.usuario = { id: user.id, email: user.email, perfil: user.perfil };
       next();
+    } catch (err) {
+      return res.status(403).json({ mensagem: "Token inválido" });
     }
-    catch (err) { return res.status(403).json({ mensagem: 'Token inválido' }); }
   };
 };
