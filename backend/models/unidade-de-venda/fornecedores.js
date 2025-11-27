b7cc6de9223e366b955a029434f4fa9348bcc491
hirowski <blue.hiro17@gmail.com>
2025-11-27 14:04:27 -0300

    apaguei os arquivos que já tinham (arquivos duplicados)

--- backend/models/unidade-de-venda/fornecedores.js
+++ backend/models/unidade-de-venda/fornecedores.js
@@ -52,3 +52,31 @@ export const verContratos = async(unidadeId)=>{
     }
   }
 }
+
+export async function updateFornecedor(id, data) {
+  try {
+    const fornecedor = await prisma.fornecedor.update({
+      where: { id },
+      data: {
+        nomeEmpresa: data.nomeEmpresa,
+        descricaoEmpresa: data.descricaoEmpresa,
+        material: data.material,
+        cnpj: data.cnpj,
+        contato: data.contato,
+        email: data.email,
+        endereco: data.endereco
+      }
+    })
+    return {
+      sucesso: true,
+      fornecedor,
+      message: "Fornecedor atualizado com sucesso!!",
+    }
+  } catch (error) {
+    return {
+      sucesso: false,
+      erro: "Erro ao atualizar fornecedor",
+      detalhes: error.message,
+    }
+  }
+}
