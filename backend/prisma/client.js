93cf843f20f59d42d5820be8bf3c6ca292327e52
RS0usa1 <richardrrggts@gmail.com>
2025-11-29 20:56:42 -0300

    ta funcionando a bagaça do animaisController

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -1,13 +1,13 @@
 // import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from './generated/client.ts';
+import { PrismaClient } from './generated/client.js';
 const globalForPrisma = globalThis;
 
 // Reaproveita a instância entre reloads (útil em dev) e entre lambdas (em teoria)
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 const prisma =
-  // globalForPrisma.prisma ||
+  globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
     log: ['error', 'warn'], // você pode adicionar 'query' se quiser debug
