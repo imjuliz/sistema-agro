6a7574e81a14850f051034a09e924ce8d57ab354
RS0usa1 <richardrrggts@gmail.com>
2025-12-11 01:03:01 -0300

    coisa

--- frontend/web/src/app/(public)/sobreNos/page.jsx
+++ frontend/web/src/app/(public)/sobreNos/page.jsx
@@ -161,26 +161,7 @@ export default function sobreNos({
         }
         setFormData((prev) => ({ ...prev, [field]: value }));
     };
-    const { lang, changeLang } = useTranslation();
-    const languageOptions = [
-        { value: 'pt-BR', label: 'Português (BR)' },
-        { value: 'en', label: 'English' },
-        { value: 'es', label: 'Español' },
-        { value: 'fr', label: 'Français' }
-    ];
-
-    const { theme: globalTheme, selectedFontSize: globalSelectedFontSize, applyPreferences } = useAppearance(); // Obter do contexto
-    // Estados locais para edição temporária antes de salvar
-    const [localTheme, setLocalTheme] = useState(globalTheme);
-    const [localSelectedFontSize, setLocalSelectedFontSize] = useState(globalSelectedFontSize);
-    const [localLang, setLocalLang] = useState(lang);
-
-    const isPreferencesDirty = localTheme !== globalTheme || localSelectedFontSize !== globalSelectedFontSize || localLang !== lang;
-    useEffect(() => {
-        setLocalTheme(globalTheme);
-        setLocalSelectedFontSize(globalSelectedFontSize);
-        setLocalLang(lang);
-    }, [globalTheme, globalSelectedFontSize, lang]);
+    
 
     return (
         <main className="container mx-auto bg-background ">
